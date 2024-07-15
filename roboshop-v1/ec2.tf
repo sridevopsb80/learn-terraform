5d0ea55fb3db69d715ce199f0ec4f12ac622e670
SVR Kalluru <raghuk.vit@gmail.com>
2024-07-15 06:55:43 +0530

    Terraform code

---
+++ roboshop-v1/ec2.tf
@@ -0,0 +1,30 @@
+resource "aws_instance" "frontend" {
+  ami           = "ami-041e2ea9402c46c32"
+  instance_type = "t3.small"
+  vpc_security_group_ids = ["sg-065fe091c4525f4dd"]
+
+  tags = {
+    Name = "frontend"
+  }
+}
+
+resource "aws_instance" "mongo" {
+  ami           = "ami-041e2ea9402c46c32"
+  instance_type = "t3.small"
+  vpc_security_group_ids = ["sg-065fe091c4525f4dd"]
+
+  tags = {
+    Name = "mongo"
+  }
+}
+
+resource "aws_instance" "catalogue" {
+  ami           = "ami-041e2ea9402c46c32"
+  instance_type = "t3.small"
+  vpc_security_group_ids = ["sg-065fe091c4525f4dd"]
+
+  tags = {
+    Name = "catalogue"
+  }
+}
+

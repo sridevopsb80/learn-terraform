b8e1c1d0ed9e505a35eaa1d919ccb1c9a774ff2e
SVR Kalluru <raghuk.vit@gmail.com>
2024-07-15 07:14:42 +0530

    Terraform code

--- 03-variables/vars.tf
+++ 03-variables/vars.tf
@@ -1,12 +1,35 @@
+# Plain variable
+
+# Number
 variable "x" {
   default = 10
 }
 
+# Boolean
+variable "y" {
+  default = true
+}
+
+# String
+variable "z" {
+  default = "Hello"
+}
+
+# Values numbers and booleans need not to be quoted
 
 
 
 # outputs
+# Direct values does not require to be accessed with ${}
 output "x" {
   value = var.x
 }
+
+# Accessing Variable has a combination with a string then we need to refer the variable with ${}
+output "x1" {
+  value = "Value of x - ${var.x}"
+}
+
+
+
 

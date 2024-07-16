08b91ebf62b548cf25c64276787d19d9fa3e5aee
SVR Kalluru <raghuk.vit@gmail.com>
2024-07-16 07:11:03 +0530

    Terraform code

---
+++ 07-conditions/main.tf
@@ -0,0 +1,10 @@
+# condition in terraform is to pick a value, Not about running the block
+
+resource "aws_route53_record" "frontend" {
+  zone_id = "Z00376861T6KFA01SJSIS"
+  name    = "test.rdevopsb80.online"
+  type    = "A"
+  ttl     = 15
+  records = [1.1.1.1]
+}
+

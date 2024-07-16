27b57f2b062116088706cf14bd303e10a3fb38ed
SVR Kalluru <raghuk.vit@gmail.com>
2024-07-16 06:41:56 +0530

    Terraform code

--- roboshop-v3/main.tf
+++ roboshop-v3/main.tf
@@ -9,13 +9,13 @@ resource "aws_instance" "instance" {
   }
 }
 
-#
-# resource "aws_route53_record" "dns_record" {
-#   count = length(var.components)
-#   zone_id = data.aws_route53_zone.zone.zone_id
-#   name    = "frontend.dev.${var.domain_name}"
-#   type    = "A"
-#   ttl     = 15
-#   records = [aws_instance.frontend.private_ip]
-# }
+
+resource "aws_route53_record" "dns_record" {
+  count = length(var.components)
+  zone_id = data.aws_route53_zone.zone.zone_id
+  name    = "${var.components[count.index]}.dev.${var.domain_name}"
+  type    = "A"
+  ttl     = 15
+  records = [aws_instance.instance.private_ip[count.index]]
+}
 

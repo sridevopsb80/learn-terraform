8c328a43b65542f18c5c5baed462fd81411ad5fb
SVR Kalluru <raghuk.vit@gmail.com>
2024-07-16 07:07:35 +0530

    Terraform code

--- roboshop-v4/variables.tf
+++ roboshop-v4/variables.tf
@@ -7,11 +7,11 @@ variable "components" {
     frontend = {
       instance_type = "t3.micro"
     }
-    catalogue = {
-      instance_type = "t3.micro"
-    }
     mongo = {
       instance_type = "t3.small"
+    }
+    catalogue = {
+      instance_type = "t3.micro"
     }
   }
 }

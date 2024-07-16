2655bbdf7a6e78483cbffc8dac0b307dd6158ade
SVR Kalluru <raghuk.vit@gmail.com>
2024-07-16 07:15:23 +0530

    Terraform code

--- 07-conditions/main.tf
+++ 07-conditions/main.tf
@@ -1,10 +1,11 @@
 # condition in terraform is to pick a value, Not about running the block
+# = expression ? TRUE_VAL : FALSE_VAL
 
 resource "aws_route53_record" "frontend" {
   zone_id = "Z00376861T6KFA01SJSIS"
   name    = "test.rdevopsb80.online"
   type    = "A"
-  ttl     = var.ttl
+  ttl     = var.ttl == "" ? 15 : var.ttl
   records = ["1.1.1.1"]
 }
 

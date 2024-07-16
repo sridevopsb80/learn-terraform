7a3b3adbb6d5a20107c19906fd71fae5e50f45ab
SVR Kalluru <raghuk.vit@gmail.com>
2024-07-16 06:36:08 +0530

    Terraform code

--- 05-loops-1/main.tf
+++ 05-loops-1/main.tf
@@ -1,3 +1,11 @@
 resource "null_resource" "test" {
   count = 3
 }
+
+variable "components" {
+  default = ["frontend", "catalogue", "mongo"]
+}
+
+resource "null_resource" "test1" {
+  count = length(var.components)
+}

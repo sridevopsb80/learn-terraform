333f7f9de6b376929ad78013753b46f0db534151
SVR Kalluru <raghuk.vit@gmail.com>
2024-07-18 06:23:57 +0530

    Terraform code

--- 09-modules-2/dns/main.tf
+++ 09-modules-2/dns/main.tf
@@ -1,6 +1,6 @@
 resource "aws_route53_record" "frontend" {
   zone_id = "Z00376861T6KFA01SJSIS"
-  name    = "test.rdevopsb80.online"
+  name    = "test1.rdevopsb80.online"
   type    = "A"
   ttl     = 15
   records = [var.private_ip]

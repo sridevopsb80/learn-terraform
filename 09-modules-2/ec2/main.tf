6b6055d027485c9128de40db11b48f2c5387d938
SVR Kalluru <raghuk.vit@gmail.com>
2024-07-18 06:16:17 +0530

    Terraform code

---
+++ 09-modules-2/ec2/main.tf
@@ -0,0 +1,10 @@
+resource "aws_instance" "test" {
+  ami           = "ami-041e2ea9402c46c32"
+  instance_type = "t3.small"
+  vpc_security_group_ids = ["sg-065fe091c4525f4dd"]
+
+  tags = {
+    Name = "test"
+  }
+}
+

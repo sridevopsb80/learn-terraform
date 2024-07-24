b41c16a3fabdf959a8901e03c9e188cbda0cefae
SVR Kalluru <raghuk.vit@gmail.com>
2024-07-24 06:43:46 +0530

    Create all ec2 instances.

---
+++ roboshop-all-ec2/variables.tf
@@ -0,0 +1,21 @@
+variable "domain_name" {
+  default = "rdevopsb80.online"
+}
+
+variable "components" {
+  default = {
+    frontend = {}
+    mongo = {}
+    catalogue = {}
+    redis = {}
+    rabbitmq = {}
+    mysql = {}
+    user = {}
+    cart = {}
+    shipping = {}
+    payment = {}
+    dispatch = {}
+  }
+}
+
+

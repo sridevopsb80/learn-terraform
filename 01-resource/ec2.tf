706bf200bdcd54856c2e65e93e8c69a1688f9c5b
sri <sriramrmk@gmail.com>
2024-08-23 12:33:00 -0400

    add comments

--- 01-resource/ec2.tf
+++ 01-resource/ec2.tf
@@ -1,3 +1,7 @@
+#resources are part of providers and can be found under documentation under providers in terraform registry.
+
+#arguments are inputs. attributes are values that can be used after the creation of the resource. for example, in case of ami, it might be the private ip.
+
 resource "aws_instance" "web" {
   ami           = "ami-041e2ea9402c46c32"
   instance_type = "t3.micro"

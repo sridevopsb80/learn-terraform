42161fb9455a4945ca9020906dc42f646d2cdeba
srikvp <93551165+srikvp@users.noreply.github.com>
2024-08-25 11:51:21 -0400

    update comments

--- 09-modules-2/main.tf
+++ 09-modules-2/main.tf
@@ -1,9 +1,11 @@
+#this is the root module. child modules(ec2, dns) are being called here. child modules can be stored locally or in vcs such as git.
+
 module "ec2" {
   source = "./ec2"
 }
 
 module "dns" {
   source     = "./dns"
-  private_ip = module.ec2.private_ip
+  private_ip = module.ec2.private_ip #output value private ip taken from ec2 module
 }
 
